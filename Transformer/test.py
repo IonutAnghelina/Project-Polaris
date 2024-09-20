96d7fb8422c91e7f5b10118fbc715065fef4bd77
ionanghelina <ionut.anghelina@newto.tech>
2024-09-20 21:28:48 +0300

    Remove ignored files from tracking and update .gitignore

--- Transformer/test.py
+++
@@ -1,7 +0,0 @@
-from transformer import Transformer
-import torch 
-
-model = Transformer(vocab_size = 100, target_vocab_size = 100, source_seq_len = 10, target_seq_len = 10, embedding_size = 512)
-source = torch.Tensor([[1,1,2,3,4,5,0,0,0,0]]).type(torch.LongTensor)
-
-print(model.decode)
